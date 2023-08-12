d8b923e8292deee91a56fc8555c259c7e6ab59e6
Mark Lavrentyev <lavrema@outlook.com>
2023-08-12 09:37:13 -0700

    Add helpers for dealing with rects

--- src/shapes/rectangle.rs
+++ src/shapes/rectangle.rs
@@ -4,15 +4,21 @@ use crate::utils::Position;
 
 
 pub struct Rectangle {
-  top_left: Position,
-  top_edge: Vector3<f64>,
-  left_edge: Vector3<f64>,
+  pub top_left: Position,
+  pub top_edge: Vector3<f64>,
+  pub left_edge: Vector3<f64>,
 }
 
 impl Rectangle {
-  pub fn bottom_left(&mut self) -> Position { self.top_left + self.left_edge }
+  pub fn bottom_left(&self) -> Position { self.top_left + self.left_edge }
 
-  pub fn top_right(&mut self) -> Position { self.top_left + self.top_edge }
+  pub fn top_right(&self) -> Position { self.top_left + self.top_edge }
 
-  pub fn bottom_right(&mut self) -> Position { self.top_left + self.top_edge + self.left_edge }
+  pub fn bottom_right(&self) -> Position { self.top_left + self.top_edge + self.left_edge }
+
+  pub fn width(&self) -> f64 { self.top_edge.norm() }
+
+  pub fn height(&self) -> f64 { self.left_edge.norm() }
+
+  pub fn center(&self) -> Position {}
 }
