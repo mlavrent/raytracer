816970f2ccf75a7335acda6cf23e6120bbe88572
Mark Lavrentyev <lavrema@outlook.com>
2023-08-12 14:21:50 -0700

    Implement scene render function

--- src/raytracer/mod.rs
+++ src/raytracer/mod.rs
@@ -1,17 +1,18 @@
 pub mod ray;
 pub mod scene;
 
-use crate::utils::Color;
-use image::ImageBuffer;
-use nalgebra::{DMatrix, vector};
+use crate::utils::{Color, color_to_eight_bit};
+use image::{RgbImage, ImageBuffer, Rgb};
+use nalgebra::vector;
 use ray::Ray;
 use scene::Scene;
 
-pub fn render_scene(scene: Scene) -> DMatrix<> {
-  scene.camera.pixel_points().map(|p| get_ray_color(scene.camera.pixel_to_ray(p), &scene))
+pub fn render_scene(scene: Scene) -> RgbImage {
+  ImageBuffer::from_fn(scene.camera.pixel_width(), scene.camera.pixel_height(),
+    |pixel_x, pixel_y| color_to_eight_bit(get_ray_color(scene.camera.get_pixel_ray(vector![pixel_x, pixel_y]), &scene)))
 }
 
 pub fn get_ray_color(ray: Ray, _: &Scene) -> Color {
   // just return the z-height of the ray as red, this should result in a gradient
-  vector![*ray.direction.into_inner().index((0, 1)), 0.0, 0.0]
+  Rgb([*ray.direction.into_inner().index((0, 1)), 0.0, 0.0])
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,14 @@
-use nalgebra::{Vector3, DMatrix, Vector2};
+use image::{Rgb, RgbImage};
+use nalgebra::{Vector3, Vector2};
 
 pub type Position = Vector3<f64>;
-pub type Color = Vector3<f64>;
+pub type Color = Rgb<f64>;
 pub type UVCoords = Vector2<f64>;
 
-pub fn export_to_file(file: &str, image_data: DMatrix<Color>) {
+pub fn color_to_eight_bit(color: Color) -> Rgb<u8> {
+  todo!()
+}
+
+pub fn export_to_file(file: &str, image_data: RgbImage) {
 
 }
