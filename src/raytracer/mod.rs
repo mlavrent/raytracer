c22889711e33bbfbb5c8b24b118202d9026282de
Mark Lavrentyev <lavrema@outlook.com>
2023-08-12 13:11:59 -0700

    Add defn for uv coords

--- src/raytracer/mod.rs
+++ src/raytracer/mod.rs
@@ -2,15 +2,16 @@ pub mod ray;
 pub mod scene;
 
 use crate::utils::Color;
-use nalgebra::DMatrix;
+use image::ImageBuffer;
+use nalgebra::{DMatrix, vector};
 use ray::Ray;
 use scene::Scene;
 
-pub fn render_scene(scene: Scene) -> DMatrix<Color> {
+pub fn render_scene(scene: Scene) -> DMatrix<> {
   scene.camera.pixel_points().map(|p| get_ray_color(scene.camera.pixel_to_ray(p), &scene))
 }
 
 pub fn get_ray_color(ray: Ray, _: &Scene) -> Color {
-  // just return the z-height of the ray, this should result in a gradient
-  ray.direction.into_inner()
+  // just return the z-height of the ray as red, this should result in a gradient
+  vector![*ray.direction.into_inner().index((0, 1)), 0.0, 0.0]
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
-use nalgebra::{Vector3, DMatrix};
+use nalgebra::{Vector3, DMatrix, Vector2};
 
 pub type Position = Vector3<f64>;
 pub type Color = Vector3<f64>;
+pub type UVCoords = Vector2<f64>;
 
-pub fn export_to_file(imageData: DMatrix<Color>) {
+pub fn export_to_file(file: &str, image_data: DMatrix<Color>) {
 
 }
