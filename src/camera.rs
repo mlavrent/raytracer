84b87e2d419c5d8e116a405f7b49c2f69a552c8d
Mark Lavrentyev <lavrema@outlook.com>
2023-08-12 09:37:00 -0700

    Add helpers for dealing with camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,16 @@
 use crate::utils::Position;
+use crate::shapes::rectangle::Rectangle;
 
 pub struct Camera {
-  eye_ppsition: Position,
-  viewport: Viewport,
+  pub eye_position: Position,
+  pub viewport: Rectangle,
+  pub pixel_width: u32,
 }
 
-pub struct Viewport {
-  top_left: Position,
-  top_right: Position,
-  bottom_left: Position,
+impl Camera {
+  pub fn focal_length(&self) -> f64 { (self.viewport.center() - self.eye_position).norm() }
+
+  pub fn aspect_ratio(&self) -> f64 { self.viewport.width() / self.viewport.height() }
+
+  pub fn pixel_height(&self) -> u32 { self.pixel_width as f64 / self.aspect_ratio() }
 }
