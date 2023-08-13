0b5b229eb3d8d05c2ae4499a4cf2f0d12a11db1b
Mark Lavrentyev <lavrema@outlook.com>
2023-08-12 17:59:15 -0700

    Implement basic ray hitting for sphere

--- src/raytracer/mod.rs
+++ src/raytracer/mod.rs
@@ -1,7 +1,7 @@
 pub mod ray;
 pub mod scene;
 
-use crate::utils::{Color, color_to_eight_bit};
+use crate::{utils::{Color, color_to_eight_bit}, shapes::Shape};
 use image::{RgbImage, ImageBuffer, Rgb};
 use nalgebra::vector;
 use ray::Ray;
@@ -13,7 +13,11 @@ pub fn render_scene(scene: Scene) -> RgbImage {
     |pixel_x, pixel_y| color_to_eight_bit(get_ray_color(scene.camera.get_pixel_ray(vector![pixel_x, pixel_y]), &scene)))
 }
 
-pub fn get_ray_color(ray: Ray, _: &Scene) -> Color {
-  // just return the z-height of the ray as red, this should result in a gradient
-  Rgb([(ray.direction.into_inner().x + 1.0) / 2.0, 0.0, 0.0])
+pub fn get_ray_color(ray: Ray, scene: &Scene) -> Color {
+  let ray_hit = scene.sphere.shape.ray_hits(&ray);
+
+  match ray_hit {
+    Some(_) => Rgb([1.0, 0.0, 0.0]),
+    None => Rgb([1.0, 1.0, -(ray.direction.into_inner().z - 1.0) / 2.0]),
+  }
 }
